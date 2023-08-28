113bcde7464874a1edcf7faa8b936bae662bf4ed
Adam Gyula Nemes <nemesgyadam@gmail.com>
2023-08-28 18:01:10 +0200

    Update ros_test.py

--- ros_test.py
+++ ros_test.py
@@ -7,8 +7,8 @@ print(commands["forward"])
 import time
 #time.sleep(3)
 continous = roslibpy.Message(
-        {"linear": {"x": 0.1, "y": 0.0, "z": 0.0}, # x előre max 0.4
-         "angular": {"x": 0.0, "y": 0.0, "z":0.0}  # z irány -0.8 jobbra vége, 0.8 balra vége
+        {"linear": {"x": 0.1, "y": 0.0, "z": 0.0}, # x forward max 0.4
+         "angular": {"x": 0.0, "y": 0.0, "z":0.0}  # z axis -0.8 right max, 0.8 left max
         })
 for i in range(100):
     print("sending:")
